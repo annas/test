99b184eda95b6e229378023f384b711d85a429d2
annas <scsswi@leeds.ac.uk>
2015-04-03 11:22:17 +0100

    Update on_success.sh

--- .travis/on_success.sh
+++ .travis/on_success.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 mvn javadoc:javadoc
+
+echo -e "Publishing javadoc...\n"
+
+  cp -R target/site/javadoc $HOME/javadoc-latest
+  
+  cd $HOME
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "travis-ci"
+  git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/annas/test gh-pages > /dev/null
+  
+  cd gh-pages
+  git rm -rf ./javadoc
+  cp -Rf $HOME/javadoc-latest ./javadoc
+  git add -f .
+  git commit -m "Lastest javadoc on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to gh-pages"
+  git push -fq origin gh-pages > /dev/null
+
+  echo -e "Published Javadoc to gh-pages.\n"
