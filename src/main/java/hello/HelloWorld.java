f52cdbd9b7742031ec92595314ba886bc54b5fdc
annas <scsswi@leeds.ac.uk>
2015-04-03 08:30:45 +0100

    Update HelloWorld.java

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -3,12 +3,12 @@ package hello;
 /**
  * Class level javadoc
  * 
- * */
+ * /
 public class HelloWorld {
     
     /**
      * Method javadoc
-     * */
+     * /
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
     }
