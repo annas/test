4b033c6c76901de9d93a9f2e0e8fd1c2df252913
annas <annas@users.noreply.github.com>
2015-04-04 16:48:36 +0100

    Create SimpleTest.java

---
+++ src/test/java/hello/SimpleTest.java
@@ -0,0 +1,20 @@
+package hello;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SimpleTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
